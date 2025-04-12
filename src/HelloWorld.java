0b35c7e9f3bf7018d9c8a4ad4508f6f7d25c10d6
karthikyashwanth <125976205+karthikyashwanth@users.noreply.github.com>
2025-04-12 18:21:39 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Dockerized Java App!");
         Thread.sleep(1000000); // Keeps app running for ~16 minutes
     }
 }
+
